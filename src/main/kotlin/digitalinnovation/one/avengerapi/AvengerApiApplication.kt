10b0371dad8b0a61cc04687b4b551d3b0650cc74
Christian <chrismoska@gmail.com>
2024-04-18 18:48:46 -0300

    init project

---
+++ src/main/kotlin/digitalinnovation/one/avengerapi/AvengerApiApplication.kt
@@ -0,0 +1,11 @@
+package digitalinnovation.one.avengerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AvengerApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AvengerApiApplication>(*args)
+}
---
+++ src/test/kotlin/digitalinnovation/one/avengerapi/AvengerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package digitalinnovation.one.avengerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
